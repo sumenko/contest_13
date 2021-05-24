13f7ee53bda46ced0be033a1367ada6455df35bf
Vladimir Sumenko <3760097@gmail.com>
2021-05-24 23:11:34 +0300

    13 in progress

--- contest_13G.py
+++ contest_13G.py
@@ -1,7 +1,10 @@
+import time
+
 class StackMax():
     def __init__(self):
         self.items = []
         self.max_item = None
+        self.max_items = []
 
     def command(self, cmd):
         parsed = cmd.split()
@@ -22,19 +25,24 @@ class StackMax():
             self.items.pop()
         except IndexError:
             print('error')
-        
+
         try:
             self.max_item = max(self.items)
         except ValueError:
             self.max_item = None
 
-
     def push(self, i):
         self.items.append(i)
-        if self.max_item == None:
-            self.max_item = i
-        elif i > self.max_item:
-            self.max_item = i
+        
+        if self.max_items:
+            self.max_items.append(i)
+            return
+
+        if i > self.max_items[-1]:
+            self.max_items.append(i)
+            return
+
+
 
 if __name__=='__main__':
     stack = StackMax()
@@ -47,9 +55,12 @@ if __name__=='__main__':
     #         outp.write(f'pop\n')
     #     for i in range(n):
     #         outp.write(f'get_max\n')
-
+    start = time.time()
     with open('input.txt', 'r') as inp:
         n = int(inp.readline())
         lines = inp.readlines()
         for i in range(n):
             stack.command(lines[i])
+
+    end = time.time()
+    print('Finished at {:.2} s'.format(end-start))
