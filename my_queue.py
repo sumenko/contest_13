ea784d92ecdb224634faeeac954493a8b00dc524
Vladimir Sumenko <3760097@gmail.com>
2021-05-29 13:32:40 +0300

    start contest_I

--- my_queue.py
+++ my_queue.py
@@ -10,13 +10,20 @@ class Queue():
         return self.size == 0
 
     def push(self, x):
-        if self.tail != self.max_n:
+        if self.size != self.max_n:
             self.queue[self.tail] = x
             self.tail = (self.tail + 1) % self.max_n
             self.size += 1
     
     def pop(self):
-        pass
+        if self.is_empty():
+            return None
+        
+        x = self.queue[self.head]
+        self.queue[self.head] = None
+        self.head = (self.head + 1) % self.max_n
+        self.size -= 1
+        return x
 
     def print(self):
         print('s{:02d} t{:02d} h{:02d} q:{}'.format(self.size, self.tail, self.head, self.queue))
@@ -24,8 +31,14 @@ class Queue():
 if __name__ == '__main__':
     q = Queue(10)
     print(q.is_empty())
-    q.print()
-    q.push(10)
-    q.push(1)
-    q.print()
-    print(q.is_empty())
+    for n in range(12):
+        q.print()
+        q.push(n)
+
+    for n in range(3):
+        print(q.pop())
+        q.print()
+
+    for n in range(1,9):
+        q.print()
+        q.push(n*3)
