e195205802ac975cc7677bc0a09c48e7f9e8c1b2
Vladimir Sumenko <3760097@gmail.com>
2021-06-03 23:49:05 +0300

    13A done (before refactor)

--- final_13A.py
+++ final_13A.py
@@ -1,80 +1,80 @@
-from typing import DefaultDict
+class QueueIsEmpty(Exception):
+    pass
+
+
+class QueueIsFull(Exception):
+    pass
 
 
 class Deque():
     def __init__(self, n):
-        self.queue_length = n
-        self.queue = [None] * n
-        self.idx_front = self.queue_length - 1
-        self.idx_back = 0
+        self._queue_length = n
+        self._queue = [None] * n
+        self.__idx_front = self._queue_length - 1
+        self.__idx_back = 0
         self.size = 0
 
     def is_empty(self):
         return self.size == 0
 
     def push_back(self, x):
-        if self.size != self.queue_length:
-            self.queue[self.idx_back] = x
-            self.idx_back = (self.idx_back + 1) % self.queue_length
+        if self.size != self._queue_length:
+            self._queue[self.__idx_back] = x
+            self.__idx_back = (self.__idx_back + 1) % self._queue_length
             self.size += 1
         else:
-            print('error')
-
+            raise QueueIsFull
 
     def pop_back(self):
         if self.size:
             self.size -= 1
-            if self.idx_back:
-                self.idx_back -= 1
+            if self.__idx_back:
+                self.__idx_back -= 1
             else:
-                self.idx_back = self.queue_length - 1
+                self.__idx_back = self._queue_length - 1
 
-            value = self.queue[self.idx_back]
-            self.queue[self.idx_back] = None
-            print(value)
-            return
-        print('error') # TODO сделать raise
+            value = self._queue[self.__idx_back]
+            self._queue[self.__idx_back] = None
+            return value
+        else:
+            raise QueueIsEmpty
 
     def push_front(self, x):
-        if self.size != self.queue_length:
-            self.queue[self.idx_front] = x
+        if self.size != self._queue_length:
+            self._queue[self.__idx_front] = x
             self.size += 1
 
-            if self.idx_front:
-                self.idx_front-=1
+            if self.__idx_front:
+                self.__idx_front -= 1
             else:
-                self.idx_front = self.queue_length - 1
-            return
-        print('error') # TODO сделать raise
-        
+                self.__idx_front = self._queue_length - 1
+        else:
+            raise QueueIsFull
 
     def pop_front(self):
         if self.size:
             self.size -= 1
-            self.idx_front = (self.idx_front + 1) % self.queue_length
-            value = self.queue[self.idx_front]
-            print(value)
-            return
-        print('error')
-
+            self.__idx_front = (self.__idx_front + 1) % self._queue_length
+            value = self._queue[self.__idx_front]
+            self._queue[self.__idx_front] = None
+            # print(value)
+            return value
+        else:
+            raise QueueIsEmpty
 
     def print(self):
-        data={'size': self.size,
-              'idx_front': self.idx_front,
-              'idx_back': self.idx_back}
-        
+        data = {'size': self.size,
+                '__idx_front': self.__idx_front,
+                '__idx_back': self.__idx_back}
+
         print('\n'.join([f'{k}: {str(data[k])}' for k in data]))
-        
+
         lst = ()
 
-        for a in self.queue:
+        for a in self._queue:
             b = f'{a:>3}' if a else '___'
             lst += (b, )
         print(*lst, sep=' | ')
-
-class QueueIsEmpty(Exception):
-    def __init__(self, msg):
-        print(msg)
 
 
 def main():
@@ -83,35 +83,20 @@ def main():
 
     if not (0 < n <= 5000 and 0 < m <= 1000):
         return
-    
+
     d = Deque(m)
     for _ in range(n):
         try:
             action, *val = input().split()
-            getattr(d, action)(*val)
+            value = getattr(d, action)(*val)
+            if value:
+                print(value)
 
-        except AttributeError:
+        except QueueIsEmpty:
             print('error')
-            break
+        except QueueIsFull:
+            print('error')
 
-def manual():
-    n = 10
-    m = 15
-    test = ('print', 'push_back 1', 'print', 'push_back 2', 'print', 'push_back 599', 'pop_back', 'print', 'pop_back', 'print')
-    d = Deque(m)
-    for t in test:
-        action, *val = t.split()
-        getattr(d, action)(*val)
-        # method = getattr(d, action)(*argc, **kwargs)
-        # if val:
-        #     method(*val)
-        # else:
-        #     method()
-
-    # d.print()
-    
-    # for name, inp, res in tests:
-    #     print(name, '\n', inp, '\n', res)
 
 if __name__ == '__main__':
     main()
--- test_final13A.py
+++ test_final13A.py
@@ -6,7 +6,7 @@ from final_13a_tests import tests_ready as tests_a
 from final_13a_tests import tests_not_ready as tests_a_not
 
 from final_13A import main as main_a
-from final_13A import Deque
+from final_13A import Deque, QueueIsFull, QueueIsEmpty
 class TestFinal13(unittest.TestCase):
     def _get_output(
         self,
@@ -31,24 +31,25 @@ class TestFinal13(unittest.TestCase):
         queue_length = 4
         d = Deque(queue_length)
         
-        self.assertEqual(d.queue_length, queue_length)
-        self.assertEqual(d.queue, [None, None, None, None])
+        self.assertEqual(d._queue_length, queue_length)
+        self.assertEqual(d._queue, [None, None, None, None])
         
         d.push_back(1)
         d.push_back(3)
         d.push_back(2)
         self.assertEqual(d.size, 3)
-        self.assertEqual(d.queue, [1, 3, 2, None])
+        self.assertEqual(d._queue, [1, 3, 2, None])
         
         d.pop_back()
         self.assertEqual(d.is_empty(), False)
-        self.assertEqual(d.queue, [1, 3, None, None])
+        self.assertEqual(d._queue, [1, 3, None, None])
         
         d.pop_back()
         d.pop_back()
-        self.assertEqual(d.queue, [None, None, None, None])
+        self.assertEqual(d._queue, [None, None, None, None])
         self.assertEqual(d.size, 0)
         self.assertEqual(d.is_empty(), True)
+
 
     def test_push_back_front(self):
         """ Test push _front _back behavior """
@@ -57,15 +58,34 @@ class TestFinal13(unittest.TestCase):
 
         d.push_back(1)
         d.push_front(4)
-        self.assertEqual(d.queue, [1, None, None, 4])
+        self.assertEqual(d._queue, [1, None, None, 4])
         d.push_front(3)
-        self.assertEqual(d.queue, [1, None, 3, 4])
+        self.assertEqual(d._queue, [1, None, 3, 4])
         d.push_back(5)
-        self.assertEqual(d.queue, [1, 5, 3, 4])
-        d.push_back(6) # ERROR raise
-        self.assertEqual(d.queue, [1, 5, 3, 4])
+        self.assertEqual(d._queue, [1, 5, 3, 4])        
+        self.assertEqual(d._queue, [1, 5, 3, 4])
         d.pop_back()
-        self.assertEqual(d.queue, [1, None, 3, 4])
+        self.assertEqual(d._queue, [1, None, 3, 4])
+
+    
+    def test_raises_errors_full(self):
+        """ Test class raises QueueIsFull """
+        d = Deque(2)
+        d.push_back(1)
+        d.push_front(2)
+        with self.assertRaises(QueueIsFull):
+            d.push_back(3)
+    
+    def test_raises_errors_empty(self):
+        """ Test class raises QueueIsEmpty """
+        d = Deque(2)
+
+        with self.assertRaises(QueueIsEmpty):
+            d.pop_back()
+        with self.assertRaises(QueueIsEmpty):
+            d.pop_front()
+
+
         
 
     def test_final_a(self):
