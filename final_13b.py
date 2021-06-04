2407a383352c9baffebdf9d740a5a2ebb92fc5ce
Vladimir Sumenko <3760097@gmail.com>
2021-06-04 17:38:36 +0300

    final_13B - done for review

--- final_13b.py
+++ final_13b.py
@@ -1,24 +1,46 @@
-from io import StringIO
-from unittest.mock import patch
+# Спринт 13. Задача B
+# Суменко В.А.
+# попытки: 51820227
 
 
-class Calculator():
-    def __init__(self, expression):
-        self.expression = expression
+def calculator(expr):
+    if expr == '':
+        return None
 
-    def calculate(self):
-        return self.expression
+    task = expr.split()
+
+    stack = []
+    action = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a // b,
+    }
+    while task:
+        x = task.pop(0)
+        if x not in '+-/*':
+            stack.append(int(x))
+        else:
+            b, a = stack.pop(), stack.pop()
+            res = action[x](a, b)
+            stack.insert(0, res)
+    result = stack.pop()
+    return result
 
 
 def main():
-    c = Calculator(input())
-    print(c.calculate())
+    expr = input()
+    print(calculator(expr))
 
 
 if __name__ == '__main__':
     tests = (
         ('2 1 + 3 *\n', 9),
-        ('7 2 + 4 * 2 +\n', 38)
+        ('7 2 + 4 * 2 +\n', 38),
+        ('1\n', 1),
+        ('0\n', 0)
     )
+    for expr, res in tests:
+        assert calculator(expr) == res
 
     main()
